60e4a3ae3d3ebb500f8f1dab5a6d6bd41d783d2c
Edgarm <edgar.martinez@canteradigital.mx>
2025-11-18 15:32:21 -0600

    Update(about me): update about me english description and also technologies tab description

--- src/constants/english.ts
+++ src/constants/english.ts
@@ -1,8 +1,8 @@
-export const SELF_DESCRIPTION_EN = `My name is Edgar Martínez, and I'm a 24-year-old computer systems engineer. I've been working as a software developer for the past few years, mainly in the frontend area. I also have a specialization in information security, which allows me to write more secure and reliable code. I really love learning new things about technology, and I enjoy reading about it, but what I like the most is applying it in real-world scenarios.`;
-export const WORKING_ON_EN = `Currently I'm learning about AWS seeking the developer certification. Also, I'm working as a freelancer on the development of an administrative software, and whenever I get the chance, I like to work on personal projects to keep improving my skills.`;
+export const SELF_DESCRIPTION_EN = `Experienced software developer with 3+ years of experience on frontend and backend technologies, focused on delivering reliable, robust and scalable web applications, ensuring a high-quality software by optimizing performance and ensuring a smooth user experience. I adapt quickly to new challenges and technologies, aiming to improve team efficiency and the final product. I also have hands-on experience integrating AWS services and AI models/tools into web applications.`;
+export const WORKING_ON_EN = `Currently interested on areas like Cloud, DevOps and AI. Looking to implement and keep working with these technologies on my next role.`;
 export const EXPERIENCE_EN = 'Experience';
 export const PROJECTS_EN = 'Personal Projects';
 export const TECHNOLOGIES_EN = 'Technologies';
 export const TASKS_EN = 'Done tasks:';
-export const TECHNOLOGIES_DESC_EN = `Here are some technologies I've worked with in frontend, backend and cybersecurity. I've barely used some of them but I always try to dive deeper into each one to understand them better. I usually enjoy working with React and SpringBoot.`;
+export const TECHNOLOGIES_DESC_EN = `During my professional career I've worked with a variety of tools and technologies, each of them bringing new perspectives, improving my learning ability and deepening my interest in both familiar and new technologies. I usually feel most comfortable working with React on the frontend and Spring Boot or Express for backend development.`;
 export const CONTACT_EN = 'Contact';
